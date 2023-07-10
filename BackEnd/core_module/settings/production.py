f721eeed05aa6b03ef24f1ab69bfe5eb49dbd695
k4st3m <eescobar0702@gmail.com>
2023-07-10 10:55:18 -0500

    asdasd

--- BackEnd/core_module/settings/production.py
+++ BackEnd/core_module/settings/production.py
@@ -1,3 +1,23 @@
+
+
+from .base import *
+import sys
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "constructo_db",
+        "USER": "postgres",
+        "PASSWORD":"@nDREE1%",
+        "HOST": "msp_db.v3ntrue.xyz",
+        "PORT":  "5432",
+    },
+    
+}
+if 'test' in sys.argv or 'test_coverage' in sys.argv: #Covers regular testing and django-coverage
+    DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
+SECRET_KEY = '7@3r)xo@*a_v+tyn7i5y8a27x8v23v8igkx9@x=86jd93838m7'
+
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES' :[
         # 'rest_framework.permissions.IsAuthenticated',
@@ -5,3 +25,39 @@ REST_FRAMEWORK = {
 
     ]
 }
+DEBUG = False
+
+ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1", "localhost", "idf.v3ntrue.xyz", ]
+
+CSRF_TRUSTED_ORIGINS = ["https://*.v3ntrue.xyz"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "filters": {"require_debug_false": {"()": "django.utils.log.RequireDebugFalse"}},
+    "handlers": {
+        "mail_admins": {
+            "level": "ERROR",
+            "filters": ["require_debug_false"],
+            "class": "django.utils.log.AdminEmailHandler",
+        },
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "django.request": {
+            "handlers": ["mail_admins"],
+            "level": "ERROR",
+            "propagate": True,
+        },
+    },
+}
+SECURE_HSTS_SECONDS = 2,592,000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
--- BackEnd/core_module/wsgi.py
+++ BackEnd/core_module/wsgi.py
@@ -1,16 +1,17 @@
 """
-WSGI config for core_module project.
+WSGI config for IDS_APP project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
-
+from whitenoise import WhiteNoise
+from django.conf import settings
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core_module.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Icore_module.settings.production")
 
-application = get_wsgi_application()
+application = WhiteNoise(get_wsgi_application(), root=settings.STATIC_ROOT)

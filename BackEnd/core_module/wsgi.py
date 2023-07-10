f270074ea98d18e6df86fe227ba75d0d9d23eb60
k4st3m <eescobar0702@gmail.com>
2023-07-10 14:46:42 -0500

    asd

--- BackEnd/core_module/wsgi.py
+++ BackEnd/core_module/wsgi.py
@@ -1,17 +1,9 @@
-"""
-WSGI config for IDS_APP project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
 from whitenoise import WhiteNoise
 from django.conf import settings
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Icore_module.settings.production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core_module.settings.production")
 
 application = WhiteNoise(get_wsgi_application(), root=settings.STATIC_ROOT)
